4e0811e565170e50d8fb110067a8c35405eb8648
tomas freilij <tomas.freilij@gmail.com>
2025-11-09 17:18:32 -0300

    FIX: Se corrigieron parámetros para poder trabajar con varios buckets

--- competencia-02/competencia.sh
+++ competencia-02/competencia.sh
@@ -1,3 +1,3 @@
 echo "config=config-prod.yml" > .env
 source ../../.venv312/bin/activate
-python competencia-02.py > ../../buckets/logs/optimizacion-$1.txt 2>&1 &
+python competencia-02.py > ../../buckets/$2/log/optimizacion-$1.txt 2>&1 &
